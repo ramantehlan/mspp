7ff5c281ad4bb9479f8ef6508f83a4d2023c5967
Raman Tehlan <ramantehlan@gmail.com>
2017-08-12 14:49:38 +0530

    Added script for sections

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,4 +1,36 @@
 $(document).ready(function(){
-		$(".welcome_section").hide();
-		$(".about_section").show();
+		$(".welcome_section").show();
+
+		$("#home").click(function(){
+			$(".selected").removeClass("selected");
+			$(this).addClass("selected");
+
+			$(".hide").hide();
+			$(".welcome_section").show();
+		});
+
+		$("#company").click(function(){
+			$(".selected").removeClass("selected");
+			$(this).addClass("selected");
+
+			$(".hide").hide();
+			$(".about_section").show();
+		});
+
+		$("#products").click(function(){
+			$(".selected").removeClass("selected");
+			$(this).addClass("selected");
+
+			$(".hide").hide();
+			$(".product_section").show();
+		});
+
+		$("#contact").click(function(){
+			$(".selected").removeClass("selected");
+			$(this).addClass("selected");
+
+			$(".hide").hide();
+			$(".contact_section").show();
+		});
+
 });
